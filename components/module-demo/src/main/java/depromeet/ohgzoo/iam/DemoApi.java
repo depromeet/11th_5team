fe12d1c11a13358d26e6d2cdfcd2038b47602ab4
zkdlu <zkdlu159951@gmail.com>
2022-03-29 23:38:06 +0900

    apply modular design

---
+++ components/module-demo/src/main/java/depromeet/ohgzoo/iam/DemoApi.java
@@ -0,0 +1,12 @@
+package depromeet.ohgzoo.iam;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoApi {
+    @GetMapping
+    public void demo() {
+
+    }
+}
