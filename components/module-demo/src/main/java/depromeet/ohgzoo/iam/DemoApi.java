9a5003b84e9dd887b24ecfd5add025f5c2d4af62
zkdlu <zkdlu159951@gmail.com>
2022-03-29 23:44:20 +0900

    test api 추가

--- components/module-demo/src/main/java/depromeet/ohgzoo/iam/DemoApi.java
+++ components/module-demo/src/main/java/depromeet/ohgzoo/iam/DemoApi.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoApi {
     @GetMapping
-    public void demo() {
-
+    public String demo() {
+        return "hello world";
     }
 }
