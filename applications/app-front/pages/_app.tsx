36d07293301c1689b8d4ff7bec30c7468de62225
sangbooom <tkdqja4164@naver.com>
2022-04-04 23:14:36 +0900

    chore: eslint 적용 및 linted-stage + husky 설정

--- applications/app-front/pages/_app.tsx
+++ applications/app-front/pages/_app.tsx
@@ -1,4 +1,5 @@
 import "@styles/globals.css";
+import React from "react";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "styled-components";
 import Head from "next/head";
