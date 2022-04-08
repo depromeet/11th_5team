c1bd4e364bfe3e1d5ffa0a6db9dc46066c7925b5
zkdlu <zkdlu159951@gmail.com>
2022-04-08 20:36:52 +0900

    feature: jwt support 추가

---
+++ components/support-jwt/src/main/java/depromeet/ohgzoo/iam/jwt/JwtServiceImpl.java
@@ -0,0 +1,67 @@
+package depromeet.ohgzoo.iam.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
+
+@Service
+public class JwtServiceImpl implements JwtService {
+    private String secret = "어느거를 키로 할까요. 알아 맞춰 봅시다. 딩동댕동";
+
+    @PostConstruct
+    protected void init() {
+        secret = Base64.getEncoder()
+                .encodeToString(secret.getBytes(StandardCharsets.UTF_8));
+    }
+
+    @Override
+    public String issuedToken(String subject, String role, long periodSecond) {
+        Claims claims = Jwts.claims().setSubject(subject);
+        claims.put("role", role);
+
+        Date now = new Date();
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(new Date(now.getTime() + periodSecond * 1000))
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+    private Key getSigningKey() {
+        byte[] keyBytes = secret.getBytes(StandardCharsets.UTF_8);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    @Override
+    public boolean verifyToken(String token) {
+        try {
+            Claims claims = getBody(token);
+            return claims.getExpiration().after(new Date());
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    @Override
+    public String getSubject(String token) {
+        Claims claims = getBody(token);
+
+        return claims.getSubject();
+    }
+
+    private Claims getBody(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+}
---
+++ components/support-jwt/src/test/java/depromeet/ohgzoo/iam/jwt/JwtServiceImplTest.java
@@ -0,0 +1,32 @@
+package depromeet.ohgzoo.iam.jwt;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class JwtServiceImplTest {
+
+    private JwtService jwtService;
+
+    @BeforeEach
+    void setUp() {
+        jwtService = new JwtServiceImpl();
+    }
+
+    @Test
+    void 토큰_만료_테스트() {
+        String expired = jwtService.issuedToken("subject", "USER", -1);
+        String valid = jwtService.issuedToken("subject", "USER", 1);
+
+        assertThat(jwtService.verifyToken(expired)).isFalse();
+        assertThat(jwtService.verifyToken(valid)).isTrue();
+    }
+
+    @Test
+    void 토큰_subject_테스트() {
+        String token = jwtService.issuedToken("subject", "USER", 1);
+
+        assertThat(jwtService.getSubject(token)).isEqualTo("subject");
+    }
+}
