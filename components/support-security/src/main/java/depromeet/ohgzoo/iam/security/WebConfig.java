5e2cf794a759434d2d864967e8bc85daaec2cb21
g1moon <gmoon.cho@gmail.com>
2022-04-20 00:17:28 +0900

    cors, oauth

--- components/support-security/src/main/java/depromeet/ohgzoo/iam/security/WebConfig.java
+++ components/support-security/src/main/java/depromeet/ohgzoo/iam/security/WebConfig.java
@@ -3,6 +3,7 @@ package depromeet.ohgzoo.iam.security;
 import depromeet.ohgzoo.iam.jwt.JwtService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -24,5 +25,11 @@ public class WebConfig implements WebMvcConfigurer {
                 .order(1)
                 .addPathPatterns("/**")
                 .excludePathPatterns(whiteList);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*");
     }
 }
