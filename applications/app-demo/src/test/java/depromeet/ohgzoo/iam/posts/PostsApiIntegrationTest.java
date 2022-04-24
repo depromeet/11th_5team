5b8e689e0d8c8095110eabf62228941d37c2fc9b
zkdlu <zkdlu159951@gmail.com>
2022-04-24 18:40:39 +0900

    인기순 게시글 조회 docs 추가

--- applications/app-demo/src/test/java/depromeet/ohgzoo/iam/posts/PostsApiIntegrationTest.java
+++ applications/app-demo/src/test/java/depromeet/ohgzoo/iam/posts/PostsApiIntegrationTest.java
@@ -19,6 +19,7 @@ public class PostsApiIntegrationTest extends IntegrationTest {
 
     @BeforeEach
     void setUp() {
+        postsRepository.deleteAll();
         postsRepository.save(new Posts(1L, PostsFirstCategory.NO1, PostsSecondCategory.NO2,
                 "content", List.of("tag1", "tag2"), false));
     }
@@ -41,6 +42,12 @@ public class PostsApiIntegrationTest extends IntegrationTest {
     void getPostsByTag() throws Exception {
         mockMvc.perform(get("/api/v1/posts/search")
                         .param("tag", "tag1"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void getPostsOrderByPopular() throws Exception {
+        mockMvc.perform(get("/api/v1/posts/popular"))
                 .andExpect(status().isOk());
     }
 }
