9432728b47f446b7161506c1d3c4a45d4aad6ac8
zkdlu <zkdlu159951@gmail.com>
2022-04-24 17:27:57 +0900

    내 글 조회 docs

--- applications/app-demo/src/test/java/depromeet/ohgzoo/iam/posts/PostsApiIntegrationTest.java
+++ applications/app-demo/src/test/java/depromeet/ohgzoo/iam/posts/PostsApiIntegrationTest.java
@@ -1,18 +1,39 @@
 package depromeet.ohgzoo.iam.posts;
 
 import depromeet.ohgzoo.iam.IntegrationTest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class PostsApiIntegrationTest extends IntegrationTest {
+
+    @Autowired
+    private PostsRepository postsRepository;
+
+    @BeforeEach
+    void setUp() {
+        postsRepository.save(new Posts(1L, PostsFirstCategory.NO1, PostsSecondCategory.NO2,
+                "content", List.of("1", "2"), false));
+    }
+
     @Test
     void createPosts() throws Exception {
         mockMvc.perform(post("/api/v1/posts")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content("{\"name\":\"geon\",\"firstCategory\":\"NO1\",\"secondCategory\":\"NO2\",\"content\":\"content\",\"tags\":[\"1\",\"2\"],\"disclosure\":false}"))
                 .andExpect(status().isCreated());
+    }
+
+    @Test
+    void getMyPosts() throws Exception {
+        mockMvc.perform(get("/api/v1/posts"))
+                .andExpect(status().isOk());
     }
 }
