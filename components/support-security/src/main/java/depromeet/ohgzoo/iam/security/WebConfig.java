74d7b38d12985897e6186017b904c31afca866fc
zkdlu <zkdlu159951@gmail.com>
2022-05-07 15:15:00 +0900

    되냐?

--- components/support-security/src/main/java/depromeet/ohgzoo/iam/security/WebConfig.java
+++ components/support-security/src/main/java/depromeet/ohgzoo/iam/security/WebConfig.java
@@ -38,7 +38,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*");
+                .allowedOrigins("http://localhost:3000");
     }
 
     @Override
