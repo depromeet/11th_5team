186f1d4dc250119d695d3a55ad1ccbd9792a2dd5
soleu <dlthf555@gmail.com>
2022-05-07 19:37:20 +0900

    test : integration test 수정

--- components/module-folder/src/main/java/depromeet/ohgzoo/iam/folder/FolderApi.java
+++ components/module-folder/src/main/java/depromeet/ohgzoo/iam/folder/FolderApi.java
@@ -14,11 +14,9 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import java.util.List;
 
 
 @RequiredArgsConstructor
