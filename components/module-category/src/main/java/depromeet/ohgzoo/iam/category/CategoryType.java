2db2d44b48abf0d896d543c0413c6a342f19f2ce
rere950303 <yhwjjang1995@naver.com>
2022-05-08 18:57:07 +0900

    fix: abstract 클래스에서 final 클래스로 변경

--- components/module-category/src/main/java/depromeet/ohgzoo/iam/category/CategoryType.java
+++ components/module-category/src/main/java/depromeet/ohgzoo/iam/category/CategoryType.java
@@ -1,11 +1,12 @@
 package depromeet.ohgzoo.iam.category;
 
-abstract class CategoryType {
+public final class CategoryType {
 
     public static final String NEGATIVE = "negative";
     public static final String POSITIVE = "positive";
     public static final String NATURAL = "natural";
 
     private CategoryType() {
+        throw new AssertionError("생성할 수 없는 클래스입니다.");
     }
 }
