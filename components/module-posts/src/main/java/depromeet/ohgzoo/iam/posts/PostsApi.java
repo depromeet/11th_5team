de022827f886ba294f0e86d5e1af8296d33af204
zkdlu <zkdlu159951@gmail.com>
2022-05-18 00:30:19 +0900

    wildcard 제거

--- components/module-posts/src/main/java/depromeet/ohgzoo/iam/posts/PostsApi.java
+++ components/module-posts/src/main/java/depromeet/ohgzoo/iam/posts/PostsApi.java
@@ -4,7 +4,13 @@ import depromeet.ohgzoo.iam.jwt.Login;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
