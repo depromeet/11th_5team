cd992a0052cf725eea1ed2eaf6eb9502213a1f26
sujin-park <psujin831@gmail.com>
2022-05-24 23:57:50 +0900

    feat: allowedOrigins vercel 배포된 도메인 추가

--- components/support-security/src/main/java/depromeet/ohgzoo/iam/security/WebConfig.java
+++ components/support-security/src/main/java/depromeet/ohgzoo/iam/security/WebConfig.java
@@ -40,7 +40,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000", "https://11th-5team-fe.vercel.app")
                 .allowedMethods("GET", "POST", "PATCH", "DELETE", "OPTIONS")
                 .allowCredentials(true);
     }
