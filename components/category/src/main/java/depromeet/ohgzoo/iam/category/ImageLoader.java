e7b31e1270dc0e1fc1f61247749e4a399280aba3
soleu <dlthf555@gmail.com>
2022-06-01 01:35:03 +0900

    chore : 디폴트이미지도 추가

--- components/category/src/main/java/depromeet/ohgzoo/iam/category/ImageLoader.java
+++ components/category/src/main/java/depromeet/ohgzoo/iam/category/ImageLoader.java
@@ -3,7 +3,7 @@ package depromeet.ohgzoo.iam.category;
 public class ImageLoader {
     public static final String base_url = "https://5gzoo.s3.ap-northeast-2.amazonaws.com/category/";
     public static final String TEMP_IMAGE = "https://firebasestorage.googleapis.com/v0/b/cardna-29f5b.appspot.com/o/20220317_172729_412500527401_720x720.png?alt=media";
-    public static final String DEFAULT_IMAGE = "https://firebasestorage.googleapis.com/v0/b/cardna-29f5b.appspot.com/o/20220317_172729_412500527401_720x720.png?alt=media";
+    public static final String DEFAULT_IMAGE = base_url + "DEFAULT.png";
     public static final String JOY_IMAGE = base_url + "JOY.png";
     public static final String PROUD_IMAGE = base_url + "PROUD.png";
     public static final String RELIEF_IMAGE = base_url + "RELIEF.png";
