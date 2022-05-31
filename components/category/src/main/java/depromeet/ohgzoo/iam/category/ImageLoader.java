8c42947adfba8a5261e875541b31716141551bad
soleu <dlthf555@gmail.com>
2022-05-31 23:20:42 +0900

    refactor : 감정 이미지 추가

--- components/category/src/main/java/depromeet/ohgzoo/iam/category/ImageLoader.java
+++ components/category/src/main/java/depromeet/ohgzoo/iam/category/ImageLoader.java
@@ -1,5 +1,19 @@
 package depromeet.ohgzoo.iam.category;
 
 public class ImageLoader {
+    public static final String base_url = "https://5gzoo.s3.ap-northeast-2.amazonaws.com/category/";
     public static final String TEMP_IMAGE = "https://firebasestorage.googleapis.com/v0/b/cardna-29f5b.appspot.com/o/20220317_172729_412500527401_720x720.png?alt=media";
+    public static final String DEFAULT_IMAGE = "https://firebasestorage.googleapis.com/v0/b/cardna-29f5b.appspot.com/o/20220317_172729_412500527401_720x720.png?alt=media";
+    public static final String JOY_IMAGE = base_url + "JOY.png";
+    public static final String PROUD_IMAGE = base_url + "PROUD.png";
+    public static final String RELIEF_IMAGE = base_url + "RELIEF.png";
+    public static final String EASYGOING_IMAGE = base_url + "EASYGOING.png";
+    public static final String CALMDOWN_IMAGE = base_url + "CALMDOWN.png";
+    public static final String LETHARGY_IMAGE = base_url + "LETHARGY.png";
+    public static final String DISAPPOINTMENT_IMAGE = base_url + "DISAPPOINTMENT.png";
+    public static final String SADNESS_IMAGE = base_url + "SADNESS.png";
+    public static final String REGRET_IMAGE = base_url + "REGRET.png";
+    public static final String IRRITATION_IMAGE = base_url + "IRRITATION.png";
+    public static final String ANXIOUS_IMAGE = base_url + "ANXIOUS.png";
+    public static final String DONTKNOW_IMAGE = base_url + "DONTKNOW.png";
 }
