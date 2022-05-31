ac071f5982207601d61379a50754e49e30249309
soleu <dlthf555@gmail.com>
2022-05-31 23:40:38 +0900

    test : 테스트 이미지 수정

--- components/module-posts/src/test/java/depromeet/ohgzoo/iam/posts/PostsApiTest.java
+++ components/module-posts/src/test/java/depromeet/ohgzoo/iam/posts/PostsApiTest.java
@@ -242,8 +242,8 @@ class PostsApiTest {
         mockMvc.perform(get("/api/v1/posts/categories"))
                 .andExpect(jsonPath("$[0].count", equalTo(1)))
                 .andExpect(jsonPath("$[0].categoryId", equalTo(11)))
-                .andExpect(jsonPath("$[0].description", equalTo("불안해요")))
-                .andExpect(jsonPath("$[0].image", equalTo("https://firebasestorage.googleapis.com/v0/b/cardna-29f5b.appspot.com/o/20220317_172729_412500527401_720x720.png?alt=media")))
+                .andExpect(jsonPath("$[0].description", equalTo(SecondCategory.ANXIOUS.getDescription())))
+                .andExpect(jsonPath("$[0].image", equalTo(SecondCategory.ANXIOUS.getImage())))
                 .andDo(print());
     }
 
