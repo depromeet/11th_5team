ac62f36064f67edde8415c08803b9e9c20321052
zkdlu <zkdlu159951@gmail.com>
2022-06-18 02:54:23 +0900

    카테고리 순서 변경

--- components/module-posts/src/main/java/depromeet/ohgzoo/iam/posts/CategoryResponse.java
+++ components/module-posts/src/main/java/depromeet/ohgzoo/iam/posts/CategoryResponse.java
@@ -4,10 +4,12 @@ import depromeet.ohgzoo.iam.category.SecondCategory;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Getter
 @NoArgsConstructor
 @EqualsAndHashCode
+@ToString
 public class CategoryResponse {
 
     int count;
