7d1521e59fbe92870f3574aa6218fba15305913b
zkdlu <zkdlu159951@gmail.com>
2022-06-26 15:55:50 +0900

    service discovery, api gateway 추가

--- applications/app-demo/src/main/java/depromeet/ohgzoo/iam/DemoApplication.java
+++ applications/app-demo/src/main/java/depromeet/ohgzoo/iam/DemoApplication.java
@@ -3,7 +3,9 @@ package depromeet.ohgzoo.iam;
 import com.zkdlu.apiresponsespringbootstarter.autoconfig.EnableResponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @EnableResponse
 @SpringBootApplication
 public class DemoApplication {
