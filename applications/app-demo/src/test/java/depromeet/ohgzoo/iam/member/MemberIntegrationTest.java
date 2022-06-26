425c3e2b2535024fa22439b884c4ce845f0866f2
zkdlu <zkdlu159951@gmail.com>
2022-06-26 12:31:09 +0900

    docs 추가

--- applications/app-demo/src/test/java/depromeet/ohgzoo/iam/member/MemberIntegrationTest.java
+++ applications/app-demo/src/test/java/depromeet/ohgzoo/iam/member/MemberIntegrationTest.java
@@ -6,12 +6,14 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.doNothing;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
@@ -25,6 +27,14 @@ public class MemberIntegrationTest extends IntegrationTest {
     @Test
     void getMySelf() throws Exception {
         mockMvc.perform(get("/users/me"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void updateMember() throws Exception {
+        mockMvc.perform(patch("/users/me")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"name\": \"new Name\"}"))
                 .andExpect(status().isOk());
     }
 
