dabfae1475cd01423777e08f2c594ce5e565b1a3
zkdlu <zkdlu159951@gmail.com>
2022-07-01 00:57:32 +0900

    왜 스텁을 하신거죠?

--- applications/app-demo/src/test/java/depromeet/ohgzoo/iam/member/MemberIntegrationTest.java
+++ applications/app-demo/src/test/java/depromeet/ohgzoo/iam/member/MemberIntegrationTest.java
@@ -5,13 +5,10 @@ import depromeet.ohgzoo.iam.IntegrationTest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.doNothing;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -20,9 +17,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @Transactional
 @ExtendWith(MockitoExtension.class)
 public class MemberIntegrationTest extends IntegrationTest {
-
-    @MockBean
-    MemberService memberService;
 
     @Test
     void getMySelf() throws Exception {
@@ -40,8 +34,6 @@ public class MemberIntegrationTest extends IntegrationTest {
 
     @Test
     void delete() throws Exception {
-        doNothing().when(memberService).delete(anyLong());
-
         mockMvc.perform(MockMvcRequestBuilders.delete("/users/me"))
                 .andExpect(status().isOk());
     }
